33198d39488b1c2cc903386fef49bb6722dac51c
ramma.fitra <rammafitracyt@gmail.com>
2023-05-09 13:58:48 +0700

    load testing

---
+++ load-testing/locust.py
@@ -0,0 +1,16 @@
+from locust import HttpUser, task, between
+import time
+
+class MyUser(HttpUser):
+    wait_time = between(1, 5)
+    counting = 0
+
+    @task
+    def submit_data(self):
+        headers = {'Content-type': 'application/json'}
+        self.counting += 1
+        data = {
+            'counting': str(self.counting),
+            'timestamp': str(time.time())
+        }
+        self.client.post('/ai_data', json=data, headers=headers)
